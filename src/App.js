5c6628aaf58d76cd9267eb69863aac0a47bafbc5
mognia <1mognia@gmail.com>
2022-03-02 16:18:58 +0330

    add and styles header login elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
 import './App.css';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
