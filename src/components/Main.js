14bda9245894819824570842efd6f845d8c67ac2
mognia <1mognia@gmail.com>
2022-03-05 01:43:24 +0330

    working on design of post layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,6 +29,60 @@ const Main = () => {
           </button>
         </div>
         </ShareBox>
+        <div>
+          <Article>
+            <SharedActor>
+              <a>
+                <img src='/images/user.svg'></img>
+                <div>
+                  <span>Title</span>
+                  <span>Info</span>
+                  <span>Date</span>
+                </div>
+              </a>
+              <button>
+                <img src='/images/ellipses.svg' />
+              </button>
+            </SharedActor>
+            <Description>Description</Description>
+            <SharedImg>
+              <a>
+                <img src='/images/sampel-img.jpg' />
+              </a>
+            </SharedImg>
+            <SocialCounts>
+              <li>
+                <button>
+                  <img src='https://static-exp1.licdn.com/sc/h/3c4dl0u9dy2zjlon6tf5jxlqo' />
+                  <img src='https://static-exp1.licdn.com/sc/h/asmf650x603bcwgefb4heo6bm' />
+                  <span>85</span>
+                </button>
+              </li>
+              <li>
+                <a>2 Comments</a>
+              </li>
+            </SocialCounts>
+            <SocialActions>
+
+            <button>
+              <img src='/images/like-icon.svg' />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src='/images/comment-icon.svg' />
+              <span>Comment</span>
+            </button>
+            <button>
+              <img src='/images/share-icon.svg' />
+              <span>Share</span>
+            </button>
+            <button>
+              <img src='/images/send-icon.svg' />
+              <span>Send</span>
+            </button>
+            </SocialActions>
+          </Article>
+        </div>
     </Container>
   )
 }
@@ -102,5 +156,129 @@ div{
   }
 }
 `
+const Article = styled(CommonCard)`
+padding:0;
+margin: 0 0 8px;
+overflow: visible;
+`
+const SharedActor = styled.div`
+padding-right: 40px;
+flex-wrap: nowrap;
+padding: 12px 16px 0;
+margin-bottom: 8px;
+align-items: center;
+display: flex;
+a{
+  margin-right: 12px;
+  flex-grow: 1;
+  overflow: hidden;
+  display: flex;
+  text-decoration: none;
+  img{
+    width: 48px;
+    height: 48px;
+  }
+  &>div {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    flex-basis: 0;
+    margin-left: 8px;
+    overflow: hidden;
+    span{
+      text-align: left;
+
+      &:first-child {
+        font-size: 14px;
+        font-weight: 700;
+        color: rgb(0,0,0,1);
+      }
+      &:nth-child(n+1) {
+        font-size: 12px;
+        color: rgba(0,0,0,0.6)
+      }
+    }
+  }
+}
+
+button {
+  position: absolute;
+  right: 12px;
+  top: 0;
+  background: transparent;
+  border: none;
+  outline: none;
+}
+`
+
+const Description = styled.div`
+padding:0 16px;
+overflow: hidden;
+color: rgba(0,0,0,0.9);
+font-size: 14px;
+text-align: left;
+`;
+const SharedImg = styled.div`
+margin-top: 8px;
+width: 100%;
+display: block;
+position: relative;
+background-color: #f9fafb;
+img {
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
+}
+`;
+const SocialCounts = styled.ul`
+    line-height: 100%;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    list-style: none;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    li {
+        margin-right: 5px;
+        font-size: 12px;
+        button {
+            display: flex;
+            border: none;
+            background: #fff;
+        }
+    }
+    img {
+        width: 18px;
+    }
+`;
+const SocialActions = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    min-height: 40px;
+    padding: 4px 8px;
+    button {
+        display: inline-flex;
+        align-items: center;
+        padding: 8px;
+        color: #0a66c2;
+        border: none;
+        background-color: #fff;
+        @media (min-width: 768px) {
+            span {
+                margin-left: 8px;
+            }
+        }
+    }
+`;
+
+const Content = styled.div`
+    text-align: center;
+    & > img {
+        width: 30px;
+    }
+`;
 
 export default Main 
