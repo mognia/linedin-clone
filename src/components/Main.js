5cbfbce9f514aafccf2bf5383b0a3a335d139c6b
mognia <1mognia@gmail.com>
2022-03-04 22:01:43 +0330

    create and design share box buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,31 @@ import styled from 'styled-components'
 const Main = () => {
   return (
     <Container>
-        <ShareBox>Share</ShareBox>
+        <ShareBox>Share
+        <div>
+          <img src='/images/user.svg' />
+          <button>Start a post</button>
+        </div>
+
+        <div>
+          <button>
+            <img src='/images/share-photo.svg' />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src='/images/share-video.svg' />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src='/images/share-event.svg' />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src='/images/share-article.svg' />
+            <span>Article</span>
+          </button>
+        </div>
+        </ShareBox>
     </Container>
   )
 }
@@ -28,6 +52,55 @@ flex-direction: column;
 color: #958b7b;
 margin: 0 0 8px; 
 background: white;
+div{
+  button{
+    outline: none;
+    color: rgba(0,0,0,0.6);
+    font-size: 14px;
+    line-height: 1.5;
+    min-height: 48px;
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+  }
+  &:first-child {
+    display: flex;
+    align-items: center;
+    padding: 8px 16px 0 16px;
+    img{
+      width: 48px;
+      border-radius: 50%;
+      margin-right:8px;
+    }
+    button{
+      margin: 4px 0;
+      flex-grow: 1;
+      border-radius: 35px;
+      padding-left: 16px;
+      border: 1px solid rgba(0,0,0,0.15);
+      border-radius: 35px;
+      background-color: white;
+      text-align: left;
+    }
+  }
+  &:nth-child(2) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding-bottom: 4px;
+
+    button{
+      img{
+        margin: 0 4px 0 -2px;
+      }
+      span{
+        color: rgba(0,0,0,0.6);
+      }
+    }
+  }
+}
 `
 
 export default Main 
